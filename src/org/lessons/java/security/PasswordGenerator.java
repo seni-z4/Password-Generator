166e04577d7b1087258f651909b9a9cce0b93b10
seniya <seniyathri@gmail.com>
2024-11-13 09:39:27 +0100

    variables added

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -2,9 +2,14 @@ package org.lessons.java.security;
 
 public class PasswordGenerator {
 
+  public String name;
+  public String surname;
+  public String color;
+  public int giorno;
+  public int mese;
+  public int anno;
+
   public static void main(String[] args) {
 
-    System.out.println("hello world is is my password");
   }
-
 }
